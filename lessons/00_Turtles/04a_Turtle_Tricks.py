6ad9d58a25ea0fe9ed9da5d6ff835b916c7cccb4
Bloody <lhickman@stmesc.org>
2024-11-06 21:25:15 +0000

    Omg

--- lessons/00_Turtles/04a_Turtle_Tricks.py
+++ lessons/00_Turtles/04a_Turtle_Tricks.py
@@ -1,28 +1,18 @@
-"""
-For this program, you will tell Tina the Turtle to draw 
-a triangle.
+import turtle
+tina = turtle.Turtle()
 
-You should look at the previous program, 02_Meet_Tina.py
-to see how to use the turtle commands.
+sides = (67589)
+angle = sides/360
+
+for i in range(sides):
+    tina.forward(150)
+    tina.left(angle)
+    
+turtle.exitonclick()
 
 
-"""
 
-# These lines are needed in most turtle programs
-import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
 
-tina = turtle.Turtle()                  # Create a turtle named tina
+tina.speed(900)
 
-# Use tina.forward(50) and tina.left(50) to draw a triangle
-# Make each side of the triangle a different color with 
-# tina.pencolor(Gold)
 
-tina.forward(50)
-tina.left(50)
-tina.pencolor(Silver)
-tina.forward(50)
-tina.pencolor(Teal)
-tina.left(50)
-turtle.exitonclick()                    # Close the window when we click on it
- 
