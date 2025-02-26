2f54fb4101ae03d4bc281ab46f0e5755516705b0
Bloody <lhickman@stmesc.org>
2025-02-26 21:52:40 +0000

    slay

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -1,31 +1,57 @@
 import turtle
-
 tina = turtle.Turtle()
-tina.right(90)
-tina.forward(50)
-tina.left(90)
-tina.forward(50)
-tina.left(90)
-tina.forward(50)
-tina.right(90)
-tina.forward(-50)
-tina.goto(0, 50)
-tina.right(90)
-tina.forward(50)
-tina.left(90)
-tina.forward(50)
-tina.left(90)
-tina.forward(50)
-tina.right(90)
-tina.forward(-50)
-tina.left(25)
+tina = turtle.Turtle()
 
-tina.forward(12.5)
-tina.left(25)
-tina.forward(25)
+import random
 
-tina.right(25)
-tina.forward(12.5)
+
+
+
+
+for i in range(5):
+    y = random.randint(-200, 200)
+    x = random.randint(-200, 200)
+    
+
+
+    tina = turtle.Turtle()
+    tina.right(90)
+    tina.forward(50)
+    tina.left(90)
+    tina.forward(50)
+    tina.left(90)
+    tina.forward(50)
+    tina.right(90)
+    tina.forward(-50)
+    tina.goto(0, 50)
+    tina.right(90)
+    tina.forward(50)
+    tina.left(90)
+    tina.forward(50)
+    tina.left(90)
+    tina.forward(50)
+    tina.right(90)
+    tina.forward(-50)
+    tina.left(25)
+
+    tina.forward(12.5)
+    tina.left(25)
+    tina.forward(25)
+            
+    tina.right(100)
+    tina.forward(17.5)
+    tina.right(-16)
+    tina.forward(21)
+    tina.penup()
+    tina.goto(x, y)
+    tina.pendown()
+
+
+
+
+
+
+
 
 speed=(2)
 
